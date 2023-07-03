71611fbf6e55abe334bcab393b313b1edb015b97
rlakmal <rruchiralakmal@gmail.com>
2023-07-03 22:37:08 +0530

    Updated Q3

--- Labsheat4/Qq2.scala
+++ Labsheat4/Qq2.scala
@@ -1,8 +1,7 @@
-
 object Qq2 {
   def main(args:Array[String]):Unit={
     print("Enter Integer number :");
-    val num = scala.io.StdIn.readInt();
+    val num = args(0).toInt;
     print("Number is :"+ patternMatching(num));
 
 
