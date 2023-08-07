a304ee34cac4df362a8d17c35fbdbde5d465bd23
rlakmal <rruchiralakmal@gmail.com>
2023-08-07 22:44:29 +0530

    labsheat 7 uploaded

---
+++ Labsheat7/L7_Q2.scala
@@ -0,0 +1,15 @@
+
+object L7_Q2 {
+    def main(args: Array[String]): Unit = {
+      val inputList = List(1, 2, 3, 4, 5)
+      val outputList = calculateSquare(inputList)
+      println(outputList)
+    }
+
+    def calculateSquare (inputList: List[Int]): List[Int] = {
+      inputList.map(number => number*number)
+    }
+}
+
+
+
