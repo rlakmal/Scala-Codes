4f008c215ee13c0271698235f088bd0428ef6570
rlakmal <rruchiralakmal@gmail.com>
2023-08-08 00:38:06 +0530

    labsheat 7 uploaded

--- Labsheat7/L7_Q3.scala
+++ Labsheat7/L7_Q3.scala
@@ -1,21 +1,16 @@
-
-object L7_Q3 {
-  def main(args: Array[String]): Unit = {
-    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val outputList = filterPrime(inputList)
-    println(outputList)
+object PrimeFilter {
+  def filterPrime(numbers: List[Int]): List[Int] = {
+    def isPrime(n: Int): Boolean = {
+      if (n <= 1) false
+      else if (n == 2) true
+      else !(2 to Math.sqrt(n).toInt).exists(x => n % x == 0)
+    }
+    numbers.filter(isPrime)
   }
 
-  def filterPrime(inputList: List[Int]): List[Int] = {
-    def isPrime(num: Int): Boolean = {
-      if (num <= 1) false
-      else if (num <= 3) true
-      else {
-        val sqrtNum = math.sqrt(num).toInt
-        !(2 to sqrtNum).exists(num % _ == 0)
-      }
-    }
-
-    inputList.filter(isPrime)
+  def main(args: Array[String]): Unit = {
+    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val primeList = filterPrime(inputList)
+    println(primeList)
   }
 }
